5083957b27c864569f940eecd42ba15c1d1835cb
Young Kim <youngkim@ground-up.kr>
2022-07-20 14:59:09 +0900

    git

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ const userController = require('../controllers');
 router.get('/', userController.baseicAPI);
 router.get('/test', userController.testAPI);
 router.post('/post_test', userController.postTestAPI);
-
+var test = "";
 
 module.exports = router;
