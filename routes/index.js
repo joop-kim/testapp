eeeed82017e14f572e8b4908c6e1c23fcf1453ae
Young Kim <youngkim@ground-up.kr>
2022-07-20 14:24:44 +0900

    00 init

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const userController = require('../controllers');
+
+router.get('/', userController.baseicAPI);
+router.get('/test', userController.testAPI);
+router.post('/post_test', userController.postTestAPI);
+
+
+module.exports = router;
